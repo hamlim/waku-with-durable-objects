58cc5c6da583a551ec7b8af6af32c0a47a8632fa
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-03-04 21:01:10 -0500

    Init

---
+++ src/lib/hono.ts
@@ -0,0 +1,20 @@
+import type { Context } from "hono";
+import { getHonoContext as getHonoContextFromWaku } from "waku/unstable_hono";
+import { isBuild } from "./waku";
+
+export function getHonoContext(): Context | undefined {
+  if (isBuild()) {
+    return undefined;
+  }
+  try {
+    return getHonoContextFromWaku<{ Bindings: Env }>();
+  } catch (e) {
+    if (isHonoContextUnavailableError(e)) {
+      return undefined;
+    }
+    throw e;
+  }
+}
+const isHonoContextUnavailableError = (e: unknown): boolean => {
+  return e instanceof Error && e.message === "Hono context is not available";
+};
---
+++ src/state.ts
@@ -0,0 +1,36 @@
+import { DurableObject } from "cloudflare:workers";
+
+export class MyDurableObject extends DurableObject {
+  private sql: DurableObjectState["storage"]["sql"];
+
+  constructor(ctx: DurableObjectState, env: Env) {
+    // Required, as we're extending the base class.
+    super(ctx, env);
+
+    this.sql = ctx.storage.sql;
+
+    this.ctx = ctx;
+    this.env = env;
+
+    this.sql.exec(`create table if not exists count(
+        value integer primary key autoincrement
+    )`);
+  }
+
+  //   async increment(): Promise<number> {
+  //     let value: number = (await this.ctx.storage.get("value")) || 0;
+  //     value += 1;
+  //     await this.ctx.storage.put("value", value);
+  //     return value;
+  //   }
+
+  async increment(): Promise<number> {
+    const result = this.sql
+      .exec<{ value: number }>(
+        "insert into count (value) values (?) returning value",
+        1,
+      )
+      .toArray();
+    return result[0]?.value || 0;
+  }
+}
